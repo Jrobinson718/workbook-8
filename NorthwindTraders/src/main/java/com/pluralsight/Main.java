3f1f540d3d855066546d60e58099a76129ca1e42
Jrobinson718 <jrobinson6284@gmail.com>
2025-06-09 14:43:34 -0400

    Feat: Using SQL connection to query northwind for all products.

---
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException,ClassNotFoundException {
+
+        String connectionString = "jdbc:mysql://localhost:3306/northwind";
+        String username = "root";
+        String password = "yearup";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection;
+        connection = DriverManager.getConnection(connectionString, username, password);
+
+        Statement statement = connection.createStatement();
+
+        String query = "SELECT ProductName FROM products";
+
+        ResultSet results = statement.executeQuery(query);
+
+        while (results.next()) {
+            String product = results.getString("ProductName");
+            System.out.println(product);
+        }
+
+        connection.close();
+    }
+}
