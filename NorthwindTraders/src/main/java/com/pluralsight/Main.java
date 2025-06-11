611856c14980d76d386589b3932072ceec3e3f5e
Jrobinson718 <jrobinson6284@gmail.com>
2025-06-10 21:24:51 -0400

    Refactor: Using the ability to take the username and password of my batabase from my command line as a different way to accedd MySQL through Java. Also utilized Prapared Statements to nullify the possibility of sql injection attacks

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -4,30 +4,46 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException,ClassNotFoundException {
+        if (args.length != 2) {
+            System.out.println("Application needs two arguments to run:" +
+                    "java com.pluralsight.Main <username> <password>");
+            System.exit(1);
+        }
 
-        String connectionString = "jdbc:mysql://localhost:3306/northwind";
-        String username = "root";
-        String password = "yearup";
+        String username = args[0];
+        String password = args[1];
 
+        displayCities(103, username, password);
+    }
+
+    public static void displayCities(int countryID, String username, String password) throws SQLException, ClassNotFoundException {
+
+        String connectionString = "jdbc:mysql://localhost:3306/sakila";
+        // Loads MySQL driver
         Class.forName("com.mysql.cj.jdbc.Driver");
 
+        // Opens a connection to the database using the url to point to the correct one.
         Connection connection;
         connection = DriverManager.getConnection(connectionString, username, password);
 
-        Statement statement = connection.createStatement();
+        // Query
+        String query = "SELECT city FROM city " +
+                "WHERE country_id = ?";
 
-        String query = """
-                SELECT ProductName\s
-                FROM products\s
-               """;
+        // Create statement
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setInt(1, countryID);
 
-        ResultSet results = statement.executeQuery(query);
+        // Executes Query
+        ResultSet results = ps.executeQuery();
 
+        // Process results
         while (results.next()) {
-            String product = results.getString("ProductName");
-            System.out.println(product);
+            String city = results.getString("city");
+            System.out.println(city);
         }
-
+        results.close();
+        ps.close();
         connection.close();
     }
 }
